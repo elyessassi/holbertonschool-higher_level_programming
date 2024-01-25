b15ffadf69757092664d71cd42c1350b4136df35
elyessassi <mjoxen@gmail.com>
2024-01-25 21:20:57 +0100

    print a string with uppercase

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def uppercase(str):
     for i in range(len(str)):
-        print(chr(ord(str[i]) - 32), end="")
-        if (i == (len(str) - 1)):
-            print("")
+        if ((ord(str[i]) >= 97) and (ord(str[i]) <= 122)):
+            print(chr(ord(str[i]) - 32), end="")
+        else:
+            print(str[i], end="")
+    print("")
+
