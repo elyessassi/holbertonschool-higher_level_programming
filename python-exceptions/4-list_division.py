25941e2e421cb3ec381195e430cc9c83b46d0ff9
elyessassi <mjoxen@gmail.com>
2024-02-01 20:12:51 +0100

    a function that divides element by element 2 lists

--- python-exceptions/4-list_division.py
+++ python-exceptions/4-list_division.py
@@ -15,4 +15,6 @@ def list_division(my_list_1, my_list_2, list_length):
         except IndexError:
             mylist.append(0)
             print("out of range")
+        finally:
+            print("", end="")
     return mylist
