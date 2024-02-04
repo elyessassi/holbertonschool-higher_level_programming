46f98f12bd25c6b39b285c54bd42f3ff71700593
elyessassi <mjoxen@gmail.com>
2024-02-04 16:25:52 +0100

    an empty class Square that defines a square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""
+an empty class Square that defines a square
+"""
+
 class Square:
-    """an empty class Square that defines a square
+    """
     an empty class Square that defines a square
     """
     pass
