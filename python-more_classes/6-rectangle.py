f643f2b0240171f07a06ce0760601b883db86c26
elyessassi <mjoxen@gmail.com>
2024-02-10 15:26:19 +0100

    adding number of instance created

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -16,7 +16,7 @@ class Rectangle:
         Rectangle.number_of_instances += 1
         self.__height = height
         self.__width = width
-        self.number_of_instances = Rectangle.number_of_instances
+        number_of_instances = Rectangle.number_of_instances
     """
     getter function for width
 
