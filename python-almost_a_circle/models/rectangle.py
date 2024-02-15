c423b7a5bbc47ebfbbe06b5a5d9d7bf24378c3f9
elyessassi <mjoxen@gmail.com>
2024-02-15 21:16:57 +0100

    rectangle class

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -14,10 +14,6 @@ class Rectangle(Base):
         x
         y
     """
-    __width = 0
-    __height = 0
-    __x = 0
-    __y = 0
 
     def __init__(self, width, height, x=0, y=0, id=None):
         """constactor that gets width height x and y"""
