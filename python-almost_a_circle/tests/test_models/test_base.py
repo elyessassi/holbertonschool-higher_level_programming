78404a5074e61f372a5513f31cef8cfabc28cc3a
elyessassi <mjoxen@gmail.com>
2024-02-15 22:34:27 +0100

    test file for base class

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""unit testing Base class"""
+
+import unittest
+import models.base
+from models.base import Base
+
+
+class Testbase(unittest.TestCase):
+    """class for unit tests"""
+    def test_case1(self):
+        """case 1"""
+        b1 = Base(10)
+        b2 = Base(17)
+        b3 = Base()
+        self.assertEqual(b1.id, 10)
+        self.assertEqual(b2.id, 17)
+        self.assertEqual(b3.id, 1)
+
+    def test_case2(self):
+        """case 2"""
+        b1 = Base()
+        b2 = Base()
+        self.assertEqual(b1.id, 2)
+        self.assertEqual(b2.id, 3)
+
+
+if __name__ == "__main__":
+    unittest.main()
