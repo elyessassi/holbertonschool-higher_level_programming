ac17da54fa50ce030617af353b45c4123e5ad784
elyessassi <mjoxen@gmail.com>
2024-02-16 12:45:33 +0100

    adding exception

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -78,3 +78,7 @@ class Rectangle(Base):
         elif value < 0:
             raise ValueError("y must be >= 0")
         self.__y = value
+
+    def area(self):
+        """a method that returns the area of the rectangle"""
+        return self.__width * self.__height
