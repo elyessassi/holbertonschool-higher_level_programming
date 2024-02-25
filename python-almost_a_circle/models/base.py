1209bc3e04417c794f7d8498d196dc7f9664e5e2
elyessassi <mjoxen@gmail.com>
2024-02-25 21:53:37 +0100

    adding to json string method

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -21,4 +21,7 @@ class Base:
 
     def to_json_string(list_dictionaries):
         """method that returns JSON format of list_dictionnaries"""
-        return json.dumps(list_dictionaries)
+        if list_dictionaries is None:
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
