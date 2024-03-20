3a90545417482c95e754149ba2b219f7a152d383
elyessassi <mjoxen@gmail.com>
2024-03-20 13:21:16 +0000

    creating a table and inserting rows to it

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- creating a table and inserting rows to it
-CREATE TABLE IF NOT EXISTS second (
+CREATE TABLE IF NOT EXISTS second_table (
   id INT,
   name VARCHAR(256),
   score INT
 );
 
-INSERT INTO second (id, name, score)
+INSERT INTO second_table (id, name, score)
 VALUES
 	(1, 'John', 10),
     (2, 'Alex', 3),
