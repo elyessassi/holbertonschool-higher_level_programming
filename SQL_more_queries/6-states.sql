8097d1cfea74302dcb37ceb3323575322453ea85
elyessassi <mjoxen@gmail.com>
2024-03-25 22:05:09 +0000

    creating data base and a table with auto incremented primary key id

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- creating data base and a table with auto incremented primary key id
-CREATE DATABASE IF NOT EXISTS hbtn_0d_usa
-USE hbtn_0d_usa
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
     id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
     name VARCHAR(256) NOT NULL
