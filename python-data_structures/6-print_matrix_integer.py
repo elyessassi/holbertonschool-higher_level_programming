15cbc2fc68370e58c5bcecba9232bb7a87b93c79
elyessassi <mjoxen@gmail.com>
2024-04-05 16:25:49 +0000

    print a matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if matrix != None:
+    if matrix is not None:
         for i in matrix:
             for j in range(len(i) - 1):
-                print("{:d} ".format(i[j]), end = "")
-            print("{:d}".format(i[len(i) - 1]) ,end = "")
-            print("")
-
-            
+                print("{:d}".format(i[j]), end = " ")
+                
