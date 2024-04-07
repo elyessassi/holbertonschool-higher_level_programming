355d67782e89a458f05f5f75583d37d7d94f5913
elyessassi <mjoxen@gmail.com>
2024-04-07 21:00:08 +0000

    reading a file

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+""" function that prints a file """
 def read_file(filename="", encoding="UTF8"):
+    """ print the content of a file """
     with open(filename) as f:
-        print(f.read())
+        print(f.read(), end="")
