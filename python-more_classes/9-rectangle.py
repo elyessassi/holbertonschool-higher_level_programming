4585669fdc4c35c79ab9b88d846e340de6df2237
elyessassi <mjoxen@gmail.com>
2025-03-27 00:28:20 +0100

    adding checks for the with and height values in the constructor method

--- python-more_classes/9-rectangle.py
+++ python-more_classes/9-rectangle.py
@@ -19,8 +19,17 @@ class Rectangle:
     """
 
     def __init__(self, width=0, height=0):
-        self.height = height
+        if isinstance(width, int) == 0:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
         self.width = width
+        if isinstance(height, int) == 0:
+            raise TypeError("height must be an integer")
+        elif height < 0:
+            raise ValueError("height must be >= 0")
+        self.height = height
+
         Rectangle.number_of_instances += 1
         self.print_symbol = Rectangle.print_symbol
 
