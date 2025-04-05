a94cd386f8fd9c0935f3d21a5bdd446516b3acee
elyessassi <mjoxen@gmail.com>
2025-04-05 02:53:20 +0100

    using super function

--- python-inheritance/10-square.py
+++ python-inheritance/10-square.py
@@ -7,7 +7,7 @@ class Square(Rectangle):
     """ Square class """
     def __init__(self, size):
         """ Constructor method """
-        self.integer_validator("size", size)
+        super().__init__(size, size)
         self.__size = size
 
     def area(self):
