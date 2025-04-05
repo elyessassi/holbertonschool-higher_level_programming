03706a5fcebdf0ea5171f64381dc3e97db5e486f
elyessassi <mjoxen@gmail.com>
2025-04-05 02:36:40 +0100

    Square Class

---
+++ python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Square Class """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class """
+    def __init__(self, size):
+        """ Constructor method """
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        """ returns the area """
+        return self.__size * self.__size
